127f9de8ca62f5f805c703dd024d191435a31352
Owen Campbell <owen.campbell@tanti.org.uk>
2022-02-09 11:43:58 +0000

    Alter copilot keymap

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,2 +1,2 @@
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-;>", "copilot#Accept('')", {noremap = true, silent = true, expr = true})
+vim.api.nvim_set_keymap("i", "<C-i>", "copilot#Accept('')", {noremap = true, silent = true, expr = true})
