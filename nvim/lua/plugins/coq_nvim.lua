cad506f2af133346307c51d8ac50b5d3d164c98f
Owen Campbell <owen.campbell@tanti.org.uk>
2022-02-09 12:19:11 +0000

    Fix coq keybindings so copilot works

--- nvim/lua/plugins/coq_nvim.lua
+++ nvim/lua/plugins/coq_nvim.lua
@@ -1,7 +1,20 @@
+local map = require("keybindings")
+
 vim.g.coq_settings = {
     auto_start = 'shut-up',
     keymap = {
         jump_to_mark = '<c-u>',
+        recommended = false,
     },
 }
 local coq = require('coq')
+
+
+
+
+vim.cmd [[
+ino <silent><expr> <Esc>   pumvisible() ? "\<C-e><Esc>" : "\<Esc>"
+ino <silent><expr> <C-c>   pumvisible() ? "\<C-e><C-c>" : "\<C-c>"
+ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
+ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
+]]
