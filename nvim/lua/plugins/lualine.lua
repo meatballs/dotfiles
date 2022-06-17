4bebc240da9877c368d9ac3794584da096916521
Owen Campbell <owen.campbell@tanti.org.uk>
2022-06-17 10:15:29 +0100

    Add tabline config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -46,7 +46,14 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
+  tabline = {
+    lualine_a = {'buffers'},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'tabs'}
+  },
   extensions = {'fugitive', 'nvim-tree'},
 }
 
