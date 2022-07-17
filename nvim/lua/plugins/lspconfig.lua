88add0bebd9f8ccc29318ac962dd905e1db76648
Owen Campbell <owen.campbell@tanti.org.uk>
2022-07-17 14:10:22 +0100

    Add marksman lsp server config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 local nvim_lsp = require("lspconfig")
 local util = require("lspconfig/util")
-local servers = {"pylsp", "yamlls", "texlab", "dockerls"}
+local servers = {"pylsp", "yamlls", "texlab", "dockerls", "marksman"}
 
 local path = util.path
 
