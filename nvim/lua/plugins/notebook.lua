2fb08b65f9df734248c215eaaa3b17650ded14de
Owen Campbell <owen.campbell@tanti.org.uk>
2023-04-13 11:39:07 +0100

    Update notebook config for latest version

--- nvim/lua/plugins/notebook.lua
+++ nvim/lua/plugins/notebook.lua
@@ -1,38 +1,28 @@
 require("notebook")
+local api = require("notebook.api")
+local settings = require("notebook.settings")
 
-function _G.define_cell()
-    local extmarks = vim.api.nvim_buf_get_var(0, "notebook.extmarks")
-    local settings = vim.api.nvim_buf_get_var(0, "notebook.settings")
-    local current_line = vim.api.nvim__buf_stats(0).current_lnum
-
-    for id, _ in pairs(extmarks) do
-        local extmark = vim.api.nvim_buf_get_extmark_by_id(
-            0, settings.plugin_namespace, id, { details = true }
-        )
-        local start_line = extmark[1] + 1
-        local end_line = extmark[3].end_row
-        if current_line >= start_line and current_line <= end_line then
-            vim.fn.MagmaDefineCell(start_line, end_line)
-            break
-        end
+function _G.define_cell(extmark)
+    if extmark == nil then
+        local line = vim.api.nvim__buf_stats(0).current_lnum
+        extmark, _ = api.current_extmark(line)
     end
+    local start_line = extmark[1] + 1
+    local end_line = extmark[3].end_row
+    pcall(function() vim.fn.MagmaDefineCell(start_line, end_line) end)
 end
 
 function _G.define_all_cells()
-    local extmarks = vim.api.nvim_buf_get_var(0, "notebook.extmarks")
-    local settings = vim.api.nvim_buf_get_var(0, "notebook.settings")
-
+    local buffer = vim.api.nvim_get_current_buf()
+    local extmarks = settings.extmarks[buffer]
     for id, cell in pairs(extmarks) do
         local extmark = vim.api.nvim_buf_get_extmark_by_id(
             0, settings.plugin_namespace, id, { details = true }
         )
         if cell.cell_type == "code" then
-            local start_line = extmark[1] + 1
-            local end_line = extmark[3].end_row
-            vim.fn.MagmaDefineCell(start_line, end_line)
+            define_cell(extmark)
         end
     end
-
 end
 
 vim.api.nvim_create_autocmd(
@@ -41,5 +31,5 @@ vim.api.nvim_create_autocmd(
 )
 vim.api.nvim_create_autocmd(
      "User",
-    {pattern = "MagmaInitPost", callback = _G.define_all_cells }
+    {pattern = {"MagmaInitPost", "NBPostRender"}, callback = _G.define_all_cells }
 )
