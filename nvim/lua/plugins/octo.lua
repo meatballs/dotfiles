5d65975f762adc87041764137c4fc8a5dae23d20
Owen Campbell <owen.campbell@tanti.org.uk>
2024-06-11 10:02:18 +0100

    Suppress octo error

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -1,4 +1,6 @@
 require("octo").setup({
-    default_remote = {"upstream", "origin", "github", "gitlab", "anvil"}
-}
-)
+    default_remote = { "upstream", "origin", "github", "gitlab", "anvil" },
+    suppress_missing_scope = {
+        projects_v2 = true,
+    }
+})
