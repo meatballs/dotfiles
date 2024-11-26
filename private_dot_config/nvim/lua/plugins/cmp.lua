5d79252b88c46ec7d5dfee7087a08f033421e29c
Owen Campbell <owen.campbell@tanti.org.uk>
2024-11-26 14:06:41 +0000

    Only accept explicitly selected completions

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -11,7 +11,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     window = {
         completion = cmp.config.window.bordered(),
