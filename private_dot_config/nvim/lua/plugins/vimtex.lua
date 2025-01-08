cc4aabf3a1e37fa8c4623144d9bbc6ebe0dcaa2c
Owen Campbell <owen.campbell@tanti.org.uk>
2025-01-08 08:16:54 +0000

    Update vimtex config to use tectonic

--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++ private_dot_config/nvim/lua/plugins/vimtex.lua
@@ -3,3 +3,4 @@ vim.g.tex_conceal              = ""
 vim.g.vimtex_fold_manual       = 1
 vim.g.vimtex_view_method       = "zathura"
 vim.g.vimtex_mappings_enabled  = false
+vim.g.vimtex_compiler_method   = "tectonic"
