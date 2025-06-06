a2c4afdbc20369738a0660d8ad599bba969c37d3
Owen Campbell <owen.campbell@empiria.co.uk>
2025-06-06 16:10:29 +0100

    Add vimtex

--- private_dot_config/lazyvim/lua/config/options.lua
+++ private_dot_config/lazyvim/lua/config/options.lua
@@ -1,22 +1,28 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local global_settings = {
-  python3_host_prog = "/home/owen/.virtualenvs/neovim/bin/python",
-  lazyvim_python_lsp = "basedpyright",
-  spelllang = "en-gb",
-}
-local options = {
-  colorcolumn = { 88 },
-  completeopt = { "menuone", "noinsert" },
-  cursorline = true,
-  guifont = "SauceCodePro Nerd Font Mono:h11",
-  hidden = true,
-  number = true,
-  shell = "/home/linuxbrew/.linuxbrew/bin/xonsh",
-  laststatus = 3,
-  winborder = "rounded",
-}
+vim.g.python3_host_prog = "/home/owen/.virtualenvs/neovim/bin/python"
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.spelllang = "en-gb"
+vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+vim.g.tex_flavor = "latex"
+vim.g.tex_conceal = ""
+vim.g.vimtex_fold_manual = 1
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_mappings_enabled = false
+vim.g.vimtex_compiler_method = "tectonic"
+
+vim.opt.colorcolumn = { 88 }
+vim.opt.completeopt = { "menuone", "noinsert" }
+vim.opt.cursorline = true
+vim.opt.guifont = "SauceCodePro Nerd Font Mono:h11"
+vim.opt.hidden = true
+vim.opt.number = true
+vim.opt.shell = "/home/linuxbrew/.linuxbrew/bin/xonsh"
+vim.opt.laststatus = 3
+vim.opt.winborder = "rounded"
+
 vim.lsp.enable({
   "cssls",
   "dockerls",
@@ -25,22 +31,12 @@ vim.lsp.enable({
   "lua_ls",
   "marksman",
   "pyslp",
-  -- "pyright",
-  -- "ruff",
   "rust_analyzer",
   "solidity_ls_nomicfoundation",
   "texlab",
   "ts_ls",
   "yamlls",
 })
-
-for k, v in pairs(global_settings) do
-  vim.g[k] = v
-end
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
 
 vim.diagnostic.config({
   virtual_text = false,
--- private_dot_config/lazyvim/lua/plugins/plugins.lua
+++ private_dot_config/lazyvim/lua/plugins/plugins.lua
@@ -45,4 +45,21 @@ return {
       },
     },
   },
+  {
+    "lervag/vimtex",
+    lazy = false, -- lazy-loading will disable inverse search
+    config = function()
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      vim.g.tex_flavor = "latex"
+      vim.g.tex_conceal = ""
+      vim.g.vimtex_fold_manual = 1
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_mappings_enabled = false
+      vim.g.vimtex_compiler_method = "tectonic"
+    end,
+    keys = {
+      { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
+    },
+  },
 }
